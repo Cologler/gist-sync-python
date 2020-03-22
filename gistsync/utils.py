33438105ccb02e69faab6f4d9463ea2e90a958b9
Cologler <skyoflw@gmail.com>
2020-03-22 18:10:55 +0800

    fixes parse gist

--- gistsync/utils.py
+++ gistsync/utils.py
@@ -16,9 +16,9 @@ def format_gist_updated_at(gist) -> str:
 def find_gist_id(input_str: str) -> str:
     'try find a gist id from user input string.'
 
-    m = re.match('https://gist.github.com/[^/]+/(.+)', input_str, re.I)
+    m = re.match(r'https://gist.github.com/([^/]+)/(?P<gist_id>.+)', input_str, re.I)
     if m:
-        return m[0]
+        return m['gist_id']
     return input_str
 
 def get_gist_version(gist) -> str:
